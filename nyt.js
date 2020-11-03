dcd43ebe05323eed899c032270cb5e7f160c2553
agul9 <57816538+agul9@users.noreply.github.com>
2020-11-03 16:54:02 -0500

    fixed it to make function work

--- nyt.js
+++ nyt.js
@@ -1,15 +1,25 @@
-//this isnt working right now :/
-//var Twit = require('twit');
-//var T = new Twit(require('./config.js'));
-//var request = require('request');
-//
-//function tweetArticle () {
-//    request('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=weather&api-key=yKQY5k4pG9hlEu9I6ND24ireqqnONnGS', function(err, response, data) {
-//        var json = JSON.parse(data);
-//        console.log("weather article from nyt");
-//        console.log(json);
-//        console.log(json.docs[0].web_url);
-//        //console.log(json.results[0].url);
-//    })
-//}
-//tweetArticle();
+//our twitter library
+var Twit = require('twit');
+//configuration file
+var T = new Twit(require('./config.js'));
+//request library
+var request = require('request');
+
+//function that gets an article related to weather from the new york times and tweets out the abstract and url link
+function tweetArticle () {
+    request('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=weather&api-key=yKQY5k4pG9hlEu9I6ND24ireqqnONnGS', function(err, response, data) {
+        if (!err) {
+            var json = JSON.parse(data);
+            let url = json.response.docs[0].web_url;
+            let summary = json.response.docs[0].abstract;
+            T.post('statuses/update', { status: summary + ": " + url }, function(err, data, response) {
+                if (!err) {
+                    console.log("just tweeted an article!"); 
+                }       
+            });
+        }
+    })
+}
+
+//calls the tweetArticle function once a week.
+//setInterval(tweetArticle, 604800000);
