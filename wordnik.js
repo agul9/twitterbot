b6b0fc8cb9130af8f1cf3c56d1884cc859c29393
agul9 <57816538+agul9@users.noreply.github.com>
2020-11-03 16:34:37 -0500

    added related word function and set intervals

--- wordnik.js
+++ wordnik.js
@@ -16,16 +16,46 @@ function weatherToday () {
     })
 }
 
-//tweets out the top example on wordnik for the word 'temperature'
-function topExample () {
-    request('https://api.wordnik.com/v4/word.json/temperature/topExample?useCanonical=false&api_key=ovl32ourxdv4d7wc86co2biof0i48nka4fvdx0qmzge5q4ymn', function(err, response, data) {
-        var json = JSON.parse(data);
-        //console.log(json.text); 
-        T.post('statuses/update', { status: json.text}, function(err, data, response) {
-            console.log("just tweeted something!");        
-        });
+//uses the word from relatedWord function to return the top example of that word
+function topExample (word) {
+    request('https://api.wordnik.com/v4/word.json/' + word +'/topExample?useCanonical=false&api_key=ovl32ourxdv4d7wc86co2biof0i48nka4fvdx0qmzge5q4ymn', function(err, response, data) {
+        if (!err) {
+            var json = JSON.parse(data);
+            //if the text is longer than a tweet then it ends with "..."
+            if ((json.text).length < 280) {
+                //let str = (((json.text).substring(0,275)) + "..."); 
+                T.post('statuses/update', { status: (((json.text).substring(0,275)) + "...")}, function(err, data, response) {
+                    if (!err) {
+                        console.log("just tweeted something!");
+                    }        
+                });
+            } else { //if text can fit as a tweet it simply tweets the whole text
+                //let str = (json.text); 
+                T.post('statuses/update', { status: json.text}, function(err, data, response) {
+                    if (!err) {
+                        console.log("just tweeted something!");
+                    }        
+                });
+            }
+        }
     })
 }
 
-//weatherToday();
-//topExample();
+//finds a word related to weather and calls the topExample function
+function relatedWord () {
+    request('https://api.wordnik.com/v4/word.json/weather/relatedWords?useCanonical=false&relationshipTypes=same-context&limitPerRelationshipType=17&api_key=ovl32ourxdv4d7wc86co2biof0i48nka4fvdx0qmzge5q4ymn', function(err, response, data) {
+        if (!err){
+            var json = JSON.parse(data);
+            //console.log(json);
+            //picks a random word in the word list
+            let x = Math.floor(Math.random() * 18);
+            console.log(json[0].words[x]);
+            topExample(json[0].words[x]); 
+        }
+    })
+}
+
+//once a day calls weatherToday function
+//setInterval(weatherToday, 86400000);
+//once every 2 days calls relatedWord function
+//setInterval(relatedWord, 172800000);
